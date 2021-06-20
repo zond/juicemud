22b96c1086feb86d9a0008223bd782fbe49f4fbd
Martin Bruse <zondolfin@gmail.com>
2021-06-20 20:58:23 +0200

    More user creation.

--- user/user.go
+++ user/user.go
@@ -13,9 +13,11 @@ var (
 
 func CreateUser(term *terminal.Terminal) (*User, error) {
 	selection := ""
+	username := ""
+	var err error
 	for selection != "y" {
-		fmt.Fprintf(term, "** Create user **\n\nEnter username:\n\n")
-		username, err := term.ReadLine()
+		fmt.Fprint(term, "** Create user **\n\nEnter new username:\n\n")
+		username, err = term.ReadLine()
 		if err != nil {
 			return nil, err
 		}
@@ -27,7 +29,30 @@ func CreateUser(term *terminal.Terminal) (*User, error) {
 			return nil, UserCreationAborted
 		}
 	}
-	fmt.Fprint(term, "OKOK!\n\n")
+	selection = ""
+	for selection != "y" {
+		fmt.Fprint(term, "Enter new password:\n\n")
+		password, err := term.ReadPassword("")
+		if err != nil {
+			return nil, err
+		}
+		fmt.Fprint(term, "Repeat new password:\n\n")
+		verification, err := term.ReadPassword("")
+		if err != nil {
+			return nil, err
+		}
+		if password == verification {
+			selection, err = termio.TerminalSelect(term, fmt.Sprintf("Create user %q with provided password?", username), []string{"y", "n", "abort"})
+			if err != nil {
+				return nil, err
+			}
+			if selection == "abort" {
+				return nil, UserCreationAborted
+			}
+		} else {
+			fmt.Fprint(term, "Passwords don't match!\n\n")
+		}
+	}
 	return &User{}, nil
 }
 
